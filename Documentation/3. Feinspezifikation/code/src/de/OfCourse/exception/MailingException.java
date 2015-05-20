b1abda2a463db61ad48b5240106f82ccece4b15d
Ricky <ricky.strohmeier@gmx.de>
2015-05-20 15:45:41 +0200

    JavaDoc Update Exceptions

--- Documentation/3. Feinspezifikation/code/src/de/OfCourse/exception/MailingException.java
+++ Documentation/3. Feinspezifikation/code/src/de/OfCourse/exception/MailingException.java
@@ -1,17 +1,17 @@
 /**
- * 
+ * Contains all custom exceptions that may occur in the OfCourse system.
  */
 package de.ofCourse.exception;
 
 /**
- * @author 
+ * Occurs if there happened a failure concering the systems mail service.
+ * E.g. the server is not reachable or a mail could not be sent.
+ *
+ * @author Ricky Strohmeier
  *
  */
 public class MailingException extends RuntimeException{
 
-    /**
-     * 
-     */
     private static final long serialVersionUID = 7556546921801557343L;
 
 }
