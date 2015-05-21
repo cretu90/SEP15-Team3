231e0c210d03a51c45b37825e9f32ef4dcf55a63
cretu90 <cretu@fim.uni-passau.de>
2015-05-21 16:12:53 +0200

    Update DatabaseGeneral

--- Code/src/de/ofCourse/Database/DatabaseGeneral/DatabaseTableCreator.java
+++ Code/src/de/ofCourse/Database/DatabaseGeneral/DatabaseTableCreator.java
@@ -1,17 +1,29 @@
 /**
- * 
+ * This package contains classes required at the launch of the system
  */
 package de.ofCourse.Database.DatabaseGeneral;
 
 import de.ofCourse.exception.InvalidDBTransferException;
 
 /**
- * @author
+ * Checks at the launch of the system if the database tables have been
+ * initialized. If not, the tables are created and initialized.
+ * 
+ * <p>
+ * This class is only required in the class <code>system.LaunchSystem</code>.
+ * 
+ * @author Patrick Cretu
  *
  */
 public class DatabaseTableCreator {
+	
     /**
-     * @throws InvalidDBTransferException
+     * Checks whether or not the required tables in the database have been
+     * initialized. If not, the necessary SQL statements are executed in order
+     * to create and initialize the tables in the database.
+     * 
+     * @throws InvalidDBTransferException if any error occurred during the
+     * execution of the method
      */
     public static void buildUpDatabase() throws InvalidDBTransferException {
     }
--- Code/src/de/ofCourse/Database/DatabaseGeneral/SetupAdmin.java
+++ Code/src/de/ofCourse/Database/DatabaseGeneral/SetupAdmin.java
@@ -1,17 +1,30 @@
 /**
- * 
+ * This package contains classes required at the launch of the system
  */
 package de.ofCourse.Database.DatabaseGeneral;
 
 import de.ofCourse.exception.InvalidDBTransferException;
 
 /**
- * @author 
+ * Checks at the launch of the system if an administrator has been initially
+ * added to the database. If not, a new administrator is created and added to
+ * the database.
+ * 
+ * <p>
+ * This class is only required in the class <code>system.LaunchSystem</code>.
+ * 
+ * @author Patrick Cretu
  *
  */
 public class SetupAdmin {
+	
     /**
-     * @throws InvalidDBTransferException
+     * Checks whether or not the table of users in the database contains an
+     * administrator. If not, a new administrator is created and added to the
+     * table containing users in the database.
+     * 
+     * @throws InvalidDBTransferException if any error occurred during the
+     * execution of the method
      */
     public static void createInitialAdmin() throws InvalidDBTransferException {
     }
