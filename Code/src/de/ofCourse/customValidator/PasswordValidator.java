131391065ef649a28a54f2e0f85037287df9263e
KathiSEP <katharina_hoelzl@web.de>
2015-05-31 18:32:28 +0200

    PasswordValidator

--- Code/src/de/ofCourse/customValidator/PasswordValidator.java
+++ Code/src/de/ofCourse/customValidator/PasswordValidator.java
@@ -3,6 +3,9 @@
  */
 package de.ofCourse.customValidator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import javax.faces.application.FacesMessage;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
@@ -11,33 +14,48 @@ import javax.faces.validator.Validator;
 import javax.faces.validator.ValidatorException;
 
 /**
- * Checks whether the inserted password fulfills certain security requirements 
- * regarding length and choice of signs. 
+ * Checks whether the inserted password fulfills certain security requirements
+ * regarding length and choice of signs.
  * 
  * @author Katharina Hölzl
  *
  */
 
-@FacesValidator("passwordvalidator")
+@FacesValidator("passwordValidator")
 public class PasswordValidator implements Validator {
 
     /**
-     * Gets called when you want to register or change your password when 
-     * you’re already registered. The method checks if the provided requirements 
-     * like usage of at least 8 signs, usage of at least one special character, 
-     * usage of at least one number and usage of lower  and upper case. 
-     * Furthermore it's not allowed to use mutations like 'ß'.
+     * Gets called when you want to register or change your password when you’re
+     * already registered. The method checks if the provided requirements like
+     * usage of at least 8 signs, usage of at least one special character, usage
+     * of at least one number and usage of lower and upper case. Furthermore
+     * it's not allowed to use mutations like 'ß'.
      */
+
+    private Pattern pattern;
+    private Matcher matcher;
+
+    private static final String PasswordPattern = "((?=.*\\d)(?=.*[a-z])"
+	    + "(?=.*[A-Z])(?=.*[@#$%]).{6,20})";
+
+    public PasswordValidator() {
+	pattern = Pattern.compile(PasswordPattern);
+    }
+
     @Override
-    public void validate(FacesContext arg0, UIComponent arg1, Object arg2)
+    public void validate(FacesContext arg0, UIComponent arg1, Object value)
 	    throws ValidatorException {
 	
-	//if(){
-	    throw new ValidatorException(new FacesMessage("Das Passwort muss "
-	    	+ "mindestens 8 Zeichen lang sein, ein Sonderzeichen, Ziffern "
-	    	+ "und Groß- und Kleinbuchstaben enthalten. ß, ä, ö, ü sind "
-	    	+ "nicht erlaubt."));
-	//}
+	matcher = pattern.matcher(value.toString());
+	
+	if (!matcher.matches()) {
+	    throw new ValidatorException(
+		    new FacesMessage(
+			    "Das Passwort muss mindestens 8 Zeichen lang sein, "
+			    + "ein Sonderzeichen, Ziffern und Groß- und "
+			    + "Kleinbuchstaben enthalten. ß, ä, ö, ü sind "
+			    + "nicht erlaubt."));
+	}
 
     }
 
