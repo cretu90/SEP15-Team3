6802c8d54f6409ead4a05f9f1bac0ca365ca1578
FuchsSEP <fuchs249@live.de>
2015-06-01 16:50:09 +0200

    CheckPhase Zwischenstand

--- Code/src/de/ofCourse/system/CheckPhase.java
+++ Code/src/de/ofCourse/system/CheckPhase.java
@@ -3,9 +3,13 @@
  */
 package de.ofCourse.system;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import javax.faces.application.NavigationHandler;
+import javax.faces.component.UIViewRoot;
+import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 import javax.faces.event.PhaseEvent;
 import javax.faces.event.PhaseId;
@@ -27,131 +31,133 @@ import javax.faces.event.PhaseListener;
  */
 public class CheckPhase implements PhaseListener {
 
-    /**
-     * Default serial version id
-     */
-    private static final long serialVersionUID = -5454041563674183939L;
+	/**
+	 * Default serial version id
+	 */
+	private static final long serialVersionUID = -5454041563674183939L;
 
-    /**
-     * List that contains the pages accessible for an anonymous user.
-     */
-    private List<String> pageListForAnonymousUsers;
+	/**
+	 * List that contains the pages accessible for an anonymous user.
+	 */
+	private List<String> pageListForAnonymousUsers;
 
-    /**
-     * List that contains the pages accessible for a registered user.
-     */
-    private List<String> pageListForRegisteredUsers;
+	/**
+	 * List that contains the pages accessible for a registered user.
+	 */
+	private List<String> pageListForRegisteredUsers;
 
-    /**
-     * List that contains the pages accessible for course leader.
-     */
-    private List<String> pageListForCourseLeaders;
+	/**
+	 * List that contains the pages accessible for course leader.
+	 */
+	private List<String> pageListForCourseLeaders;
 
-    /**
-     * List that contains the pages accessible for an system administrator.
-     */
-    private List<String> pageListForAdministrators;
+	/**
+	 * List that contains the pages accessible for an system administrator.
+	 */
+	private List<String> pageListForAdministrators;
 
-    /**
-     * Constructor of the class <code>CheckPhase</code>.<br>
-     * It fills the lists with the pages a user with a specific user role is
-     * allowed to access.
-     */
-    public CheckPhase() {
-	// TODO: CheckPhase Construtor
-    }
+	/**
+	 * Constructor of the class <code>CheckPhase</code>.<br>
+	 * It fills the lists with the pages a user with a specific user role is
+	 * allowed to access.
+	 */
+	public CheckPhase() {
 
-    /**
-     * Handles the check whether the user has the required rights to get to the
-     * requested page.<br>
-     * If the user has this rights, he is directed to the requested page.<br>
-     * Otherwise he is redirected to the start page of the system.
-     * 
-     * @param event
-     *            the phase events
-     */
-    @Override
-    public void afterPhase(PhaseEvent event) {
-	// TODO CheckPhase afterPhase()
+	}
 
-    }
+	/**
+	 * Handles the check whether the user has the required rights to get to the
+	 * requested page.<br>
+	 * If the user has this rights, he is directed to the requested page.<br>
+	 * Otherwise he is redirected to the start page of the system.
+	 * 
+	 * @param event
+	 *            the phase events
+	 */
+	@Override
+	public void afterPhase(PhaseEvent event) {
 
-    /**
-     * Handle a notification that the processing for a particular phase of the
-     * request processing life cycle is about to begin.
-     * 
-     * @param event
-     *            the phase events
-     */
-    @Override
-    public void beforePhase(PhaseEvent event) {
-	// TODO CheckPhase beforePhase()
+		// Fetches the FacesContext parameters
+		FacesContext fctx = event.getFacesContext();
+		ExternalContext ctx = fctx.getExternalContext();
+		Map<String, Object> sessionMap = ctx.getSessionMap();
+		String servletPath = ctx.getRequestServletPath();
 
-    }
+		// Is the user on the login page?
+		boolean loginPage = false;
+		UIViewRoot viewRoot = fctx.getViewRoot();
+		if (viewRoot != null) {
+			loginPage = fctx.getViewRoot().getViewId().endsWith("authenticate.xhtml");
+		}
 
-    /**
-     * Returns the identifier of the request processing phase during which this
-     * listener is interested in processing PhaseEvent events.
-     * 
-     * @return the id of the <code>RESTORE_VIEW</code> phase
-     */
-    @Override
-    public PhaseId getPhaseId() {
-	return PhaseId.RESTORE_VIEW;
-    }
+		// Is the user already authenticated?
+		boolean loggedIn = sessionMap.containsKey("loggedin");
+		
+		//Fetch data from session
+		
 
-    /**
-     * Redirects to the specified path.
-     * 
-     * @param redirectPath
-     *            The path to redirect to
-     */
-    private void redirect(String redirectPath) {
-	FacesContext context = FacesContext.getCurrentInstance();
-	NavigationHandler navHandler = context.getApplication()
-		.getNavigationHandler();
-	navHandler.handleNavigation(context, null, redirectPath
-		+ "?faces-redirect=true");
-    }
+	
+		
 
-    /**
-     * @param context
-     * @return
-     */
-    private boolean isAccessPermittedAnonymous(FacesContext context) {
-	boolean permitted = false;
-	//TODO: CheckPhase isPermitted
-	return permitted;
-    }
+	}
 
-    /**
-     * @param context
-     * @return
-     */
-    private boolean isAccessPermittedRegistered(FacesContext context) {
-	boolean permitted = false;
-	//TODO: CheckPhase isPermitted
-	return permitted;
-    }
+	/**
+	 * Handle a notification that the processing for a particular phase of the
+	 * request processing life cycle is about to begin.
+	 * 
+	 * @param event
+	 *            the phase events
+	 */
+	@Override
+	public void beforePhase(PhaseEvent event) {
+		// TODO CheckPhase beforePhase()
 
-    /**
-     * @param context
-     * @return
-     */
-    private boolean isAccessPermittedCourseLeader(FacesContext context) {
-	boolean permitted = false;
-	//TODO: CheckPhase isPermitted
-	return permitted;
-    }
+	}
 
-    /**
-     * @param context
-     * @return
-     */
-    private boolean isAccessPermittedAdministrator(FacesContext context) {
-	boolean permitted = false;
-	//TODO: CheckPhase isPermitted
-	return permitted;
-    }
+	/**
+	 * Returns the identifier of the request processing phase during which this
+	 * listener is interested in processing PhaseEvent events.
+	 * 
+	 * @return the id of the <code>RESTORE_VIEW</code> phase
+	 */
+	@Override
+	public PhaseId getPhaseId() {
+		return PhaseId.RESTORE_VIEW;
+	}
+
+	/**
+	 * Redirects to the specified path.
+	 * 
+	 * @param redirectPath
+	 *            The path to redirect to
+	 */
+	private void redirect(String redirectPath) {
+		FacesContext context = FacesContext.getCurrentInstance();
+		NavigationHandler navHandler = context.getApplication()
+				.getNavigationHandler();
+		navHandler.handleNavigation(context, null, redirectPath
+				+ "?faces-redirect=true");
+	}
+
+	/**
+	 * Returns whether the given url contains a String from a list.
+	 * 
+	 * @param pages
+	 *            list of Strings
+	 * @param url
+	 *            the url as String
+	 * @return <code>true</code>, if the url contains one of the Strings<br>
+	 *         <code>false</code>, otherwise
+	 */
+	private boolean urlContainsPageName(ArrayList<String> pages, String url) {
+		boolean contains = false;
+
+		for (int i = 0; i < pages.size() && !contains; ++i) {
+			if (url.contains(pages.get(i))) {
+				contains = true;
+			}
+		}
+		return contains;
+	}
 
 }
